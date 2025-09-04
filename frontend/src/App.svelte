1058bab40f1691acdfb8257641a4194a47c78cfb
muhlenlogan8 <muhlenlogan8@gmail.com>
2025-09-04 12:46:46 -0400

    Created svelte project and added tailwindcss

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from "./lib/Header.svelte"
+  import Counter from "./lib/Counter.svelte"
+</script>
+
+<main>
+  <Header />
+  <p class="text-2xl text-blue-500">Test!</p>
+  <Counter />
+</main>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+</style>
---
+++ frontend/src/lib/Header.svelte
@@ -0,0 +1,4 @@
+<header class="bg-blue-500 flex items-center justify-between px-6 py-2">
+    <h1 class="text-3xl text-white font-bold">My Tracker</h1>
+    <p class="text-xl text-white font-bold">User's Name</p>
+</header>
